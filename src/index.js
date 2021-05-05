516fa1c28ce0295cb8567ac45e33847ee78f00c2
Tiago <tiagospost@gmail.com>
2021-05-05 21:17:36 +0200

    Finished challenge

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 
-const { v4: uuidv4, validate } = require('uuid');
+const { v4: uuidv4, validate: uuidValidate } = require('uuid');
 
 const app = express();
 app.use(express.json());
@@ -10,19 +10,86 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+
+  const { username } = request.headers
+  const user = users.find(user => user.username === username)
+
+  if (!user) {
+    return response.status(404).json({ error: "User not found" })
+  }
+
+  request.user = user
+
+  return next()
+
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+
+  const { user } = request;
+
+  console.log(user.todos.length)
+
+  const userIsPro = user.pro === true
+  const userHasTodosAvailable = user.todos.length < 10
+
+  if (!(userIsPro || userHasTodosAvailable)) {
+    return response.status(403).json({ error: "Free plan only allow 10 todos" })
+  }
+
+  return next()
+
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+
+  const { username } = request.headers
+  const { id } = request.params
+
+
+  const user = users.find(user => user.username === username)
+
+  if (!user) {
+    return response.status(404).json({ error: "User not found" })
+  }
+
+
+  const isUuid = uuidValidate(id);
+  if (!isUuid) {
+    return response.status(400).json({ error: "id should be uuid" })
+  }
+
+
+  const todo = user.todos.find(todo => todo.id === id)
+
+
+  if (!todo) {
+    return response.status(404).json({ error: "Todo not found" })
+  }
+
+  request.user = user
+  request.todo = todo
+  return next()
+
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params
+
+  const isUuid = uuidValidate(id);
+  if (!isUuid) {
+    return response.status(400).json({ error: "id should be uuid" })
+  }
+
+  const user = users.find(user => user.id === id)
+
+  if (!user) {
+    return response.status(404).json({ error: "User not found" })
+  }
+
+  request.user = user
+
+  return next()
 }
 
 app.post('/users', (request, response) => {
@@ -87,6 +154,7 @@ app.post('/todos', checksExistsUserAccount, checksCreateTodosUserAvailability, (
 
   return response.status(201).json(newTodo);
 });
+
 
 app.put('/todos/:id', checksTodoExists, (request, response) => {
   const { title, deadline } = request.body;
